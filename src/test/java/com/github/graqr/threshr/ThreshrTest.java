a7c149c5ecd994234c5f8b010412be27a683a554
jonathan zollinger <jonathan.zollinger@outlook.com>
2023-11-17 08:41:59 -0700

    feature(test): flesh out tests per #12
    fixes #12
    
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/test/java/com/github/graqr/threshr/ThreshrTest.java
+++ src/test/java/com/github/graqr/threshr/ThreshrTest.java
@@ -2,11 +2,13 @@ package com.github.graqr.threshr;
 
 import com.github.graqr.threshr.model.TargetStore;
 import com.github.graqr.threshr.model.TcinList;
+import com.github.graqr.threshr.model.products.Products;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
 @MicronautTest
@@ -22,6 +24,11 @@ public class ThreshrTest {
     String testApiKey = "9f36aeafbe60771e321a7cc95a78140772ab3e96";
 
     Predicate<HttpResponse<?>> okResponse = response -> response.code() >= 200 && response.code() < 300;
+    Predicate<HttpResponse<Products>> productsCount = response -> 2 == Objects.requireNonNull(response.body())
+            .data()
+            .productSummary()
+            .size();
+
 
     @BeforeEach
     void setUp() {
