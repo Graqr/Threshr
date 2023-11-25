20b6bbf7197806d5676d1cb3049765a55ff2c8ba
jonathan zollinger <jonathan.zollinger@outlook.com>
2023-11-25 00:36:10 -0700

    squash me: add plpSearch endpoint and redsky metadata class
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/github/graqr/threshr/model/HarvestedProduct.java
+++ src/main/java/com/github/graqr/threshr/model/HarvestedProduct.java
@@ -2,18 +2,23 @@ package com.github.graqr.threshr.model;
 
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
-import io.micronaut.data.annotation.GeneratedValue;
-import io.micronaut.data.annotation.Id;
-import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @MappedEntity
 public record HarvestedProduct(
-        // https://micronaut-projects.github.io/micronaut-data/latest/guide/#javaRecords
-        @Id @GeneratedValue @Nullable Long id,
+        @Id @GeneratedValue @Nullable
+        Long id,
+
+        @AutoPopulated(updateable = false)
+        DateCreated dateCreated,
+
         @NonNull @NotBlank @Size(min = 10000000, max = 99999999)
         long tcin, // target's ID system
+
         @NonNull @NotBlank
-        String title) {
+        String title,
+
+        double price) {
 }
---
+++ src/main/java/com/github/graqr/threshr/model/targetProducts/Price.java
@@ -0,0 +1,29 @@
+package com.github.graqr.threshr.model.targetProducts;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.serde.annotation.Serdeable;
+
+@Serdeable
+public record Price(
+        @JsonProperty("formatted_comparison_price")
+        String formattedComparisonPrice,
+
+        @JsonProperty("formatted_comparison_price_type")
+        String formattedComparisonPriceType,
+
+        @JsonProperty("formatted_current_price")
+        String formattedCurrentPrice,
+
+        @JsonProperty("formatted_current_price_type")
+        String formattedCurrentPriceType,
+
+        @Nullable @JsonProperty("reg_retail") //for some reason this isn't always provided in target's response
+        Double price,
+
+        @JsonProperty("location_id")
+        Long locationId,
+
+        @Nullable String urgency) {
+
+}
