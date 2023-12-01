45a741600e674738844fdc5b69e16de7edda60ba
jonathan zollinger <jonathan.zollinger@outlook.com>
2023-11-30 21:53:21 -0700

    wip: progressed from 404 to 400 error
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/github/graqr/threshr/ThreshrController.java
+++ src/main/java/com/github/graqr/threshr/ThreshrController.java
@@ -1,9 +1,9 @@
 package com.github.graqr.threshr;
 
-import com.github.graqr.threshr.model.RedskyQueryMetadata;
 import com.github.graqr.threshr.model.TargetStore;
-import com.github.graqr.threshr.model.TcinList;
+import com.github.graqr.threshr.model.Tcin;
 import com.github.graqr.threshr.model.redsky.ApiResponseData;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.core.async.annotation.SingleResult;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
@@ -16,26 +16,28 @@ public class ThreshrController {
     @Inject
     ThreshrClient threshrClient;
 
+    @Property(name = "threshr.CHANNEL")
+    String CHANNEL;
+
 
     @Get("/summary")
     @SingleResult
     public HttpResponse<ApiResponseData> fetchProductSummaries(
-            RedskyQueryMetadata metadata,
-            String apiToken,
-            TcinList tcins,
-            TargetStore targetStore) {
-        return threshrClient.productSummaryWithFulfillment(metadata, apiToken, tcins, targetStore);
+            TargetStore targetStore,
+            Tcin... tcins) {
+        return threshrClient.productSummaryWithFulfillment(
+                targetStore,
+                CHANNEL,
+                tcins);
     }
 
 
     @Get("/search")
     @SingleResult
     public HttpResponse<ApiResponseData> search(
-            RedskyQueryMetadata metadata,
-            String apiToken,
-            TcinList tcins,
+            Tcin tcins,
             TargetStore targetStore) {
-        return threshrClient.plpSearch(metadata, apiToken, tcins, targetStore);
+        return threshrClient.plpSearch("I'm a key", tcins, targetStore);
     }
 
 }
--- src/test/groovy/com/github/graqr/threshr/ThreshrClientTest.groovy
+++ src/test/groovy/com/github/graqr/threshr/ThreshrClientTest.groovy
@@ -1,14 +1,14 @@
 package com.github.graqr.threshr
 
+//note: this test enables logging the actual http requests being sent
 class ThreshrClientTest extends ThreshrTest {
 
     void "no error calling product_summary_with_fulfillment_v1 endpoint"() {
         when:
         threshrClient.productSummaryWithFulfillment(
-                metadata,
-                redskyToken,
-                tcinList,
-                targetStore
+                targetStore,
+                "WEB",
+                tcinList
         )
 
         then:
