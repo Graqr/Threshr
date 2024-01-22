5dab96620c31fed2dd3ad8eee66f6433eadcd559
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2024-01-21 20:16:53 -0700

    style: clean whitespace
    Signed-off-by: Jonathan Zollinger <jonathan.zollinger@gmail.com>

--- src/main/java/com/graqr/threshr/model/redsky/products/Product.java
+++ src/main/java/com/graqr/threshr/model/redsky/products/Product.java
@@ -8,11 +8,14 @@ import java.util.List;
 @Serdeable
 public record Product(
         @JsonProperty("__typename") String typename,
-        String tcin, Category category,
-        @JsonProperty("ratings_and_reviews") RatingsAndReviews ratingsAndReviews, Item item,
+        String tcin,
+        Category category,
+        @JsonProperty("ratings_and_reviews") RatingsAndReviews ratingsAndReviews,
+        Item item,
         @JsonProperty("finds_stories")
         List<FindsStory> findsStories,
         @JsonProperty("finds_posts")
         List<FindsPost> findsPosts,
-        Price price, List<?> promotions) {
+        Price price,
+        List<?> promotions) {
 }
