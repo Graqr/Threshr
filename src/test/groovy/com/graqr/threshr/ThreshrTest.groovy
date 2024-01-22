27cdbdd3ba1b0852c6b0d0db2056f0658543c81c
Jonathan Zollinger <jonathan.zollinger@gmail.com>
2024-01-21 21:49:40 -0700

    fix: deserialize product details object
    Signed-off-by: Jonathan Zollinger <jonathan.zollinger@gmail.com>

--- src/test/groovy/com/graqr/threshr/ThreshrTest.groovy
+++ src/test/groovy/com/graqr/threshr/ThreshrTest.groovy
@@ -2,15 +2,10 @@ package com.graqr.threshr
 
 import com.graqr.threshr.model.TargetStore
 import com.graqr.threshr.model.Tcin
-import io.micronaut.context.ApplicationContext
-import io.micronaut.context.env.Environment
-import io.micronaut.http.HttpResponse
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import spock.lang.Shared
 import spock.lang.Specification
-
-import java.util.function.Predicate
 
 @MicronautTest
 class ThreshrTest extends Specification {
@@ -24,9 +19,6 @@ class ThreshrTest extends Specification {
     ThreshrClient threshrClient
 
     @Shared
-    ApplicationContext ctx
-
-    @Shared
     TargetStore targetStore = new TargetStore(
             "1750",
             "UT",
@@ -38,16 +30,5 @@ class ThreshrTest extends Specification {
     Tcin tcin = new Tcin(
             new String[]{"82691535", "12953464"} //corn & coke https://bit.ly/45V8dui https://bit.ly/40j4A0e
     )
-
-    @Shared
-    Predicate<HttpResponse<?>> okResponse = response -> response.code() >= 200 && response.code() < 300
-
-    void setupSpec() {
-        ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
-    }
-
-    void cleanupSpec() {
-        ctx.stop()
-    }
 
 }
