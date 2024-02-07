2479c848a93bf396043ebdd18c1abd72eb4da003
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-02-07 15:08:55 -0700

    test: add store query tests

--- src/test/groovy/com/graqr/threshr/ThreshrClientSpec.groovy
+++ src/test/groovy/com/graqr/threshr/ThreshrClientSpec.groovy
@@ -1,12 +1,15 @@
 package com.graqr.threshr
 
+import com.graqr.threshr.model.queryparam.Place
 import com.graqr.threshr.model.queryparam.TargetStorePdpSearch
 import com.graqr.threshr.model.redsky.products.pdp.client.PdpClientRoot
 import com.graqr.threshr.model.redsky.products.summary.ProductSummaryRoot
+import com.graqr.threshr.model.redsky.stores.NearbyStoresRoot
 import io.micronaut.http.HttpResponse
 
+import java.util.stream.Collectors
 /**
- * This test class is necessary despite similarity to the controller test. please dont delete this as the
+ * This test class is necessary despite similarity to the controller test. please don't delete this as the
  * httpclient logs are visible in this test but not in the controller test.
  */
 class ThreshrClientSpec extends ThreshrSpec {
@@ -21,7 +24,6 @@ class ThreshrClientSpec extends ThreshrSpec {
         response.body().data().productSummary().size() == tcin.tcins.split(",").size()
     }
 
-
     void "no error calling pdp client search"() {
         when:
         HttpResponse<PdpClientRoot> response = threshrClient.productDetails(
@@ -32,11 +34,24 @@ class ThreshrClientSpec extends ThreshrSpec {
         noExceptionThrown()
         null != response.body().data().product()
     }
-//
-//    void "no error querying target store locations"() {
-//        when:
-//        HttpResponse<TargetStoreRoot> response = threshrClient.queryTargetStore()
-//
-//        then:
-//    }
+
+    void 'querying "#place.getPlace()" returns the "#expectedLocationName" store'() {
+        when:
+        HttpResponse<NearbyStoresRoot> response = threshrClient.queryNearbyStores(5,100, place.getPlace())
+
+        then:
+        response.body().data().nearbyStores().stores()
+                .stream()
+                .map(it -> it.locationName())
+                .collect(Collectors.toList())
+                .contains(expectedLocationName)
+
+        where:
+        store << expectedStores
+        expectedLocationName = store.locationName()
+        place = 0 == new Random().nextInt(2)
+                ? new Place(store.mailingAddress().postalCode())
+                : new Place(store.mailingAddress().city(), store.mailingAddress().state())
+    }
+
 }
--- src/test/groovy/com/graqr/threshr/ThreshrSpec.groovy
+++ src/test/groovy/com/graqr/threshr/ThreshrSpec.groovy
@@ -1,15 +1,27 @@
 package com.graqr.threshr
 
+import com.graqr.threshr.model.queryparam.Place
 import com.graqr.threshr.model.queryparam.TargetStore
 import com.graqr.threshr.model.queryparam.Tcin
+import com.graqr.threshr.model.redsky.stores.NearbyStores
+import com.graqr.threshr.model.redsky.stores.NearbyStoresRoot
+import com.graqr.threshr.model.redsky.stores.Store
+import io.micronaut.context.annotation.Value
+import io.micronaut.core.io.ResourceLoader
+import io.micronaut.serde.ObjectMapper
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
+import net.datafaker.Faker
 import spock.lang.Shared
 import spock.lang.Specification
+
+import java.util.stream.Collectors
+import java.util.stream.Stream
 
 @MicronautTest
 class ThreshrSpec extends Specification {
 
+    //  ----------- Resources / Fields -----------
     @Inject
     @Shared
     Threshr threshrController
@@ -18,7 +30,49 @@ class ThreshrSpec extends Specification {
     @Shared
     ThreshrClient threshrClient
 
-    @Shared // TODO configure test data to be provided, not hardcoded
+    @Shared
+    Faker faker = new Faker()
+    @Shared
+    List<String> zipCodesPlus4 = Stream.generate(faker.address()::zipCodePlus4).distinct().limit(50).collect(Collectors.toList())
+    @Shared
+    List<String> zipCodes = Stream.generate(faker.address()::zipCode).distinct().limit(50).collect(Collectors.toList())
+    @Shared
+    List<String> cities = Stream.generate(faker.address()::cityName).limit(50).collect(Collectors.toList())
+    @Shared
+    List<String> states = Stream.generate(faker.address()::state).limit(50).collect(Collectors.toList())
+    @Shared
+    List<String> emptyStrings = Collections.nCopies(26, "").toList() + (Collections.nCopies(26, " "))
+
+    @Inject @Shared
+    ResourceLoader resourceLoader
+
+    @Inject @Shared
+    ObjectMapper mapper
+
+    @Shared @Value('${threshr.test.data.stores}') // $env:THRESHR_TEST_DATA_STORES
+    String storesFilePath
+
+    @Shared
+    List<Store> expectedStores
+
+    void setupSpec() {
+        expectedStores = getExpectedLocations(String.format("classpath:%s", storesFilePath))
+    }
+
+    List<Store> getExpectedLocations(String filePath) {
+        mapper.readValue(getResourceFromFile(filePath), NearbyStoresRoot.class).data().nearbyStores().stores()
+    }
+
+    byte[] getResourceFromFile(String filepath) {
+        try {
+            return resourceLoader.getResourceAsStream(filepath).get().readAllBytes()
+        } catch (NoSuchElementException e) {
+            throw new ThreshrException(String.format("failed to load '%s' for %s.", filepath, this.class.name), e)
+        }
+    }
+
+    @Shared
+    // TODO configure test data to be provided, not hardcoded
     TargetStore targetStore = new TargetStore(
             "1750",
             "UT",
@@ -31,4 +85,24 @@ class ThreshrSpec extends Specification {
             new String[]{"82691535", "12953464"} //corn & coke https://bit.ly/45V8dui https://bit.ly/40j4A0e
     )
 
+    //  ------------------ Tests ------------------
+
+    void 'querying "#place.getPlace()" returns the "#expectedLocationName" store'() {
+        when:
+        NearbyStores response = threshrController.queryStoreLocations(place)
+
+        then:
+        response.stores()
+                .stream()
+                .map(it -> it.locationName())
+                .collect(Collectors.toList())
+                .contains(expectedLocationName)
+
+        where:
+        store << expectedStores
+        expectedLocationName = store.locationName()
+        place = 0 == new Random().nextInt(2)
+                ? new Place(store.mailingAddress().postalCode())
+                : new Place(store.mailingAddress().city(), store.mailingAddress().state())
+    }
 }
