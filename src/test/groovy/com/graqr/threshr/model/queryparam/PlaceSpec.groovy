3556a3864cb5f6688d2cf64252b880e708da1adb
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-02-15 22:36:56 -0700

    refactor(readability): extract faker list method
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/test/groovy/com/graqr/threshr/model/queryparam/PlaceSpec.groovy
+++ src/test/groovy/com/graqr/threshr/model/queryparam/PlaceSpec.groovy
@@ -1,12 +1,12 @@
 package com.graqr.threshr.model.queryparam
 
-import com.graqr.threshr.ThreshrSpec
-import com.graqr.threshr.model.queryparam.Place
+
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import net.datafaker.Faker
 import spock.lang.Shared
 import spock.lang.Specification
 
+import java.util.function.Function
 import java.util.stream.Collectors
 import java.util.stream.Stream
 
@@ -16,22 +16,22 @@ class PlaceSpec extends Specification {
     @Shared
     Faker faker = new Faker()
 
+    Function<Stream<String>, List<String>> getDistinctList = stream ->
+            stream.distinct().limit(50).collect(Collectors.toList())
+
     // TODO: https://g.co/gemini/share/8af67cd35757
     @Shared
-    List<String> zipCodesPlus4 = Stream.generate(faker.address()::zipCodePlus4)
-            .distinct()
-            .limit(50)
-            .collect(Collectors.toList())
+    List<String> zipCodesPlus4 = getDistinctList(faker.address()::zipCodePlus4)
 
     @Shared
-    List<String> zipCodes = Stream.generate(faker.address()::zipCode)
-            .distinct()
-            .limit(50)
-            .collect(Collectors.toList())
+    List<String> zipCodes = getDistinctList(faker.address()::zipCode)
+
     @Shared
-    List<String> cities = Stream.generate(faker.address()::cityName).limit(50).collect(Collectors.toList())
+    List<String> cities = getDistinctList(faker.address()::cityName)
+
     @Shared
-    List<String> states = Stream.generate(faker.address()::state).limit(50).collect(Collectors.toList())
+    List<String> states = getDistinctList(faker.address()::state)
+
     @Shared
     List<String> emptyStrings = Collections.nCopies(26, "").toList() + (Collections.nCopies(26, " "))
 
