ca604e3d8bc3c4fad7d04bd7cca13fa15297b816
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-02-16 00:08:32 -0700

    fix: set new pojo's as 'serdeable'
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/graqr/threshr/model/redsky/store/CapabilityHour.java
+++ src/main/java/com/graqr/threshr/model/redsky/store/CapabilityHour.java
@@ -1,9 +1,11 @@
 package com.graqr.threshr.model.redsky.store;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.micronaut.serde.annotation.Serdeable;
 
 import java.util.List;
 
+@Serdeable
 public record CapabilityHour(@JsonProperty("capability_code") String capabilityCode,
                              List<Day> days) {
 }
--- src/main/java/com/graqr/threshr/model/redsky/store/Miscellaneous.java
+++ src/main/java/com/graqr/threshr/model/redsky/store/Miscellaneous.java
@@ -1,5 +1,7 @@
 package com.graqr.threshr.model.redsky.store;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.micronaut.serde.annotation.Serdeable;
 
+@Serdeable
 public record Miscellaneous(@JsonProperty("google_cid") String googleCid) {}
