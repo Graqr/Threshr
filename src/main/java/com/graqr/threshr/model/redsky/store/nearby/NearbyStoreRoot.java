8d147631bc7ac48e8329086b648d2cb7e20f9179
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-02-16 00:08:32 -0700

    fix: set new pojo's as 'serdeable'
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/graqr/threshr/model/redsky/store/nearby/NearbyStoreRoot.java
+++ src/main/java/com/graqr/threshr/model/redsky/store/nearby/NearbyStoreRoot.java
@@ -1,8 +1,10 @@
 package com.graqr.threshr.model.redsky.store.nearby;
 
+import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
+@Introspected
 public record NearbyStoreRoot(Data data) {
 }
 
