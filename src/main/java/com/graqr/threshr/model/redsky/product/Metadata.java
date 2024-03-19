ee34320e2194c06ffcccc5b053a35996dd571bd2
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-03-19 16:40:27 -0600

    fix: fix metadata equals method
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/graqr/threshr/model/redsky/product/Metadata.java
+++ src/main/java/com/graqr/threshr/model/redsky/product/Metadata.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 
 @Serdeable
 public record Metadata(
@@ -24,4 +26,20 @@ public record Metadata(
         int totalResults,
         @JsonProperty("sort_options")
         Map<String, String> sortOptions) {
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (Objects.isNull(object) || getClass() != object.getClass()) return false;
+        Metadata castObject = (Metadata) object;
+        return Objects.equals(count, castObject.count()) &&
+                Arrays.equals(responseIds, castObject.responseIds()) &&
+                Objects.equals(currentPage, castObject.currentPage()) &&
+                Objects.equals(keyword, castObject.keyword()) &&
+                Objects.equals(offset, castObject.offset()) &&
+                Objects.equals(sortBy, castObject.sortBy()) &&
+                Objects.equals(totalPages, castObject.totalPages()) &&
+                Objects.equals(totalResults, castObject.totalResults()) &&
+                Objects.equals(sortOptions, castObject.sortOptions());
+    }
 }
