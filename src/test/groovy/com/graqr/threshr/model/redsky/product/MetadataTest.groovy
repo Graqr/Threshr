2de16022fd14e13b725d75f250c7826e284a081e
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-03-19 17:54:30 -0600

    fix: fix metadata hashcode method
    improve similar tests in SearchRecommendationsSpec
    
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/test/groovy/com/graqr/threshr/model/redsky/product/MetadataTest.groovy
+++ src/test/groovy/com/graqr/threshr/model/redsky/product/MetadataTest.groovy
@@ -36,7 +36,7 @@ class MetadataSpec extends Specification {
     }
 
     void setupSpec() {
-        faker = new Faker();
+        faker = new Faker()
         def starWars = faker.starWars()
         responseIds = Collections.nCopies(getRandInt(), faker.idNumber().valid()).toArray(String[]::new)
         count = getRandInt()
@@ -55,12 +55,15 @@ class MetadataSpec extends Specification {
         Metadata metadata = new Metadata(
                 responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions)
 
-        expect:
+        when:
         metadata == metadata
 
         and:
         metadata == new Metadata(
                 responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions)
+
+        then:
+        noExceptionThrown()
     }
 
     void "equals comparison between an object and itself fails after changing #changedObject"() {
@@ -68,8 +71,11 @@ class MetadataSpec extends Specification {
         Metadata metadata = new Metadata(
                 responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions)
 
-        expect:
+        when:
         metadata != nonMatchingMetada
+
+        then:
+        noExceptionThrown()
 
         where:
         nonMatchingMetada                                                                                               | changedObject
@@ -90,7 +96,72 @@ class MetadataSpec extends Specification {
         new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, totalPages, getRandInt(totalResults),
                 sortOptions)                                                                                            | "totalResults"
         new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults,
-                sortOptions.replace(sortOptions.keySet().last(), faker.chuckNorris().fact()) as Map<String, String>)    | "sortOptions via edit"
+                Arrays.stream(Collections.nCopies(getRandInt(), faker.studioGhibli().character())).collect(
+                        Collectors.toMap(person -> (String) person, person -> faker.chuckNorris().fact())))             | "sortOptions"
+        Map.of("responseIds", responseIds,
+                "count", count,
+                "currentPage", currentPage,
+                "keyword", keyword,
+                "offset", offset,
+                "sortBy", sortBy,
+                "totalPages", totalPages,
+                "totalResults", totalResults,
+                "sortOptions", sortOptions)                                                                             | "everything to a Map of values matching the original object"
+        null                                                                                                            | "to a null value"
+    }
+
+    void "generated hashes of self and identical metaData object match"() {
+        given:
+        Metadata metadata = new Metadata(
+                responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions)
+
+        when:
+        metadata.hashCode() == metadata.hashCode()
+
+        and:
+        metadata.hashCode() == new Metadata(
+                responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions).hashCode()
+
+        then:
+        noExceptionThrown()
+    }
+
+    void "generated hashes of self and foreign object altered in #changedObject do NOT match"() {
+        given:
+        int ogHash = new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults,
+                sortOptions).hashCode()
+
+        when:
+        ogHash != nonMatchingMetada.hashCode()
+
+        then:
+        noExceptionThrown()
+
+        where:
+        nonMatchingMetada                                                                                               | changedObject
+        new Metadata(Collections.nCopies(faker.number().randomDigitNotZero(), faker.idNumber().valid()).toArray(String[]::new),
+                count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions)                     | "responseIds"
+        new Metadata(Collections.nCopies(faker.number().randomDigitNotZero(), null).toArray(String[]::new),
+                count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions)                     | "responseIds as null"
+        new Metadata(responseIds, (int) randIntNot.apply(count),
+                currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions)                            | "count"
+        new Metadata(responseIds, count, getRandInt(currentPage),
+                keyword, offset, sortBy, totalPages, totalResults, sortOptions)                                         | "currentPage"
+        new Metadata(responseIds, count, currentPage, faker.chuckNorris().fact(),
+                offset, sortBy, totalPages, totalResults, sortOptions)                                                  | "keyword"
+        new Metadata(responseIds, count, currentPage, keyword, getRandInt(offset),
+                sortBy, (totalPages), totalResults, sortOptions)                                                        | "offset"
+        new Metadata(responseIds, count, currentPage, keyword, offset, faker.studioGhibli().quote(),
+                totalPages, (totalResults), sortOptions)                                                                | "sortBy"
+        new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, getRandInt(totalPages),
+                totalResults, sortOptions)                                                                              | "totalPages"
+        new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, totalPages, getRandInt(totalResults),
+                sortOptions)                                                                                            | "totalResults"
+        new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults, sortOptions
+                .replace(((Map<String, String>) sortOptions)
+                        .keySet()
+                        .last(), faker.chuckNorris()
+                        .fact()) as Map<String, String>)                                                                | "sortOptions via edit"
         new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults,
                 sortOptions.remove(sortOptions.keySet().last()) as Map<String, String>)                                 | "sortOptions via trim"
         new Metadata(responseIds, count, currentPage, keyword, offset, sortBy, totalPages, totalResults,
@@ -107,7 +178,5 @@ class MetadataSpec extends Specification {
                 "totalPages", totalPages,
                 "totalResults", totalResults,
                 "sortOptions", sortOptions)                                                                             | "everything to a Map of values matching the original object"
-        null | "to a null value"
     }
-
 }
