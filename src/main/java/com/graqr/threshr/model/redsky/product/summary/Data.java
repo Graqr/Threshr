30754ed08a9fd59007e206d31abe56a0785e97b4
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-03-25 13:46:16 -0600

    refactor: correct class names
    correct plurality and conciseness in class names
    
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/graqr/threshr/model/redsky/product/summary/Data.java
+++ src/main/java/com/graqr/threshr/model/redsky/product/summary/Data.java
@@ -1,14 +1,16 @@
 package com.graqr.threshr.model.redsky.product.summary;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.graqr.threshr.model.redsky.product.ProductSummary;
+import com.graqr.threshr.model.redsky.product.ProductSummaryWithFulfillment;
 import io.micronaut.serde.annotation.Serdeable;
 
 import java.util.List;
 
 @Serdeable
-public record Data(@JsonProperty("product_summaries") List<ProductSummary> productSummary) {
-
+public record Data(
+        @JsonProperty("product_summaries")
+        List<ProductSummaryWithFulfillment> productSummaryWithFulfillmentList
+) {
 }
 
 
