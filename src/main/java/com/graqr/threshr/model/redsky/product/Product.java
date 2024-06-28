1ffe63091b7b23a1b83386f2eb6d21c066015363
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-06-27 22:15:01 -0600

    wip(test): introduce controller test for plp query
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/graqr/threshr/model/redsky/product/Product.java
+++ src/main/java/com/graqr/threshr/model/redsky/product/Product.java
@@ -1,6 +1,7 @@
 package com.graqr.threshr.model.redsky.product;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
 
 import java.util.List;
@@ -9,13 +10,12 @@ import java.util.List;
 public record Product(
         @JsonProperty("__typename") String typename,
         String tcin,
+        @Nullable @JsonProperty("original_tcin") String originalTcin,
         Category category,
         @JsonProperty("ratings_and_reviews") RatingsAndReviews ratingsAndReviews,
         Item item,
-        @JsonProperty("finds_stories")
-        List<FindsStory> findsStories,
-        @JsonProperty("finds_posts")
-        List<FindsPost> findsPosts,
+        @JsonProperty("finds_stories") List<FindsStory> findsStories,
+        @Nullable @JsonProperty("finds_posts") List<FindsPost> findsPosts,
         Price price,
         List<?> promotions) {
 }
--- src/main/java/com/graqr/threshr/model/redsky/product/Search.java
+++ src/main/java/com/graqr/threshr/model/redsky/product/Search.java
@@ -1,6 +1,7 @@
 package com.graqr.threshr.model.redsky.product;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
@@ -8,6 +9,11 @@ public record Search(
         @JsonProperty("search_recommendations")
         SearchRecommendations searchRecommendations,
         @JsonProperty("search_response")
-        SearchResponse searchResponse
+        SearchResponse searchResponse,
+        @Nullable
+        @JsonProperty("search_suggestions")
+        String[] searchSuggestions,
+        @Nullable
+        Product[] products
 ) {
 }
